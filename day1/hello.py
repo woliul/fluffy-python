005343e224eff0fefa96c18deb0ee59e0c5162ca
Md Woliul Hasan <6556302+woliul@users.noreply.github.com>
2025-07-21 21:36:27 +0600

    Create hello.py

---
+++ day1/hello.py
@@ -0,0 +1,13 @@
+# Day 1: The print() Function
+
+# This function is used to display output to the console.
+# It's your program's way of "talking" to the user!
+
+print("Hello, LinkedIn!")
+print("Python is fun!")
+
+# You can also print numbers directly
+print(123)
+
+# Or even the result of a simple calculation
+print(5 + 3)
