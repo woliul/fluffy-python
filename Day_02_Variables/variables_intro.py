547ec84dbe174d5522a6e8f9ddcb213af3e34462
Md Woliul Hasan <6556302+woliul@users.noreply.github.com>
2025-07-22 23:36:23 +0600

    Create variables_intro.py

---
+++ Day_02_Variables/variables_intro.py
@@ -0,0 +1,38 @@
+# Day 2: Variables - Storing Information
+
+# Variables are like labeled boxes where you can store different types of data.
+# You give the box a name, and then you put something inside it.
+
+# 1. Storing a String (text)
+user_name = "Woliul"
+greeting = 'Hello, Python Learner!'
+print("User Name:", user_name)
+print("Greeting:", greeting)
+
+# 2. Storing an Integer (whole number)
+age = 30
+number_of_lessons = 2
+print("Age:", age)
+print("Lessons completed:", number_of_lessons)
+
+# 3. Storing a Float (decimal number)
+price = 19.99
+pi_value = 3.14159
+print("Product Price:", price)
+print("Value of Pi:", pi_value)
+
+# 4. Storing a Boolean (True/False value)
+is_learning_python = True
+has_finished_course = False
+print("Is learning Python?", is_learning_python)
+print("Has finished course?", has_finished_course)
+
+# You can also change the value of a variable
+current_mood = "Happy"
+print("Current mood (before change):", current_mood)
+current_mood = "Excited about Python!"
+print("Current mood (after change):", current_mood)
+
+# Variables can be used in calculations
+total_cost = price * number_of_lessons
+print("Total cost of lessons:", total_cost)
